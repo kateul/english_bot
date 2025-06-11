84121699faf7bfc9ec540b4232724f6bdf828e3b
kateul <gultribble@gmail.com>
2025-06-11 11:48:23 +0200

    Обновлён config.py: ADMIN_IDS читается из .env

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMIN_IDS = os.getenv("ADMIN_IDS")
+#ADMIN_IDS = os.getenv("ADMIN_IDS")
+ADMIN_IDS = set(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+
 TEACHER_USERNAME = os.getenv("TEACHER_USERNAME")
 SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME")
