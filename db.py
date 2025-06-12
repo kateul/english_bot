05c8351dda7b03677050f40092ddaf2511f8693c
kateul <gultribble@gmail.com>
2025-06-12 10:05:37 +0200

    Updated db.py : connecting to the database via environment variables

--- db.py
+++ db.py
@@ -9,7 +9,8 @@ async def create_pool():
         port=DB_PORT,
         database=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD
+        password=DB_PASSWORD,
+        ssl='require'
     )
 
 
